5f1824e4069c807f542cf9c181d70b8dd2a8a243
tomas9807 <tomas9807@protonmail.com>
2018-07-09 14:36:12 -0400

    puta

--- catalog/urls.py
+++ catalog/urls.py
@@ -2,4 +2,7 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index,name='index'),
+    # path('books/', views.books,name='books'),
+    # path('authors/', views.authors,name='author'),
+   
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,22 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from . import models
 # Create your views here.
+def generate_context(**kwargs):
+    context = {}
+    for k,v in kwargs.items():
+        context[k] = v
+    return context
+
+
 def index(request):
-    return HttpResponse('index')
+    num_books = models.Book.objects.all().count()
+    num_instances = models.BookInstance.objects.all().count()
+    # Available books (status = 'a')
+    num_instances_available = models.BookInstance.objects.filter(status__exact='a').count()
+    num_authors = models.Author.objects.all().count()
+
+    return render(request,
+    'catalog/index.html',
+    generate_context(num_books=num_books,num_authors=num_authors,num_instances=num_instances,num_instances_available=num_instances_available)
+    )
+    
