ac753377d8b10ab06870c2a8b10ca009e6778fba
tomas9807 <tomas9807@protonmail.com>
2018-07-09 12:07:26 -0400

    perro

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,18 +1,33 @@
 from django.contrib import admin
 from . import models
 
+class BookInstanceInline(admin.TabularInline):
+    model = models.BookInstance
 #classes
 @admin.register(models.Author)
 class AuthorAdmin(admin.ModelAdmin):
     list_display = ('second_name','first_name','date_of_birth', 'date_of_death')
+    fields = ['first_name','second_name',('date_of_birth','date_of_death')]
 
 @admin.register(models.Book)
 class BookAdmin(admin.ModelAdmin):
     list_display  = ('title','author','summary','display_genre','isbn','lenguage')
+    inlines = [BookInstanceInline]
 
 @admin.register(models.BookInstance)
 class BookInstanceAdmin(admin.ModelAdmin):
-    pass
+    list_display = ('id','book','due_back','status')
+    list_filter = ('status','due_back')
+    fieldsets = (
+        (None,{
+            'fields':('book','imprint')
+        }),
+        ('Availability', {
+            'fields': ('status','due_back')
+        })
+    )
+    
+   
 
 @admin.register(models.Genre)
 class GenreAdmin(admin.ModelAdmin):
